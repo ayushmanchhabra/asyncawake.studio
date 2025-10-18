b1d58f2ed8d5903358e2dd1732dd6cf0eb816318
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-10-18 20:45:08 +0530

    chore(content): try resolving img paths

--- src/screens/Post.tsx
+++ src/screens/Post.tsx
@@ -4,6 +4,14 @@ import { useParams } from "react-router-dom";
 
 import { usePost } from "../providers";
 
+import {
+    IMG_20251001_1,
+    IMG_20251001_2,
+    IMG_20251001_3,
+    IMG_20251001_4,
+    IMG_20251001_5,
+    IMG_20251001_6
+} from '../assets/index';
 import posts from '../content/posts.json';
 
 export default function Post() {
@@ -12,10 +20,28 @@ export default function Post() {
 
     const { post } = usePost(date);
 
+    const images: Record<string, string> = {
+        '2025-10-01_1.jpeg': IMG_20251001_1,
+        '2025-10-01_2.jpeg': IMG_20251001_2,
+        '2025-10-01_3.jpeg': IMG_20251001_3,
+        '2025-10-01_4.jpeg': IMG_20251001_4,
+        '2025-10-01_5.jpeg': IMG_20251001_5,
+        '2025-10-01_6.jpeg': IMG_20251001_6,
+    };
+
     return (
         <Box style={{ display: 'block' }}>
             <div style={{ textAlign: 'left' }}>
-                <Markdown>{post}</Markdown>
+                <Markdown
+                    components={{
+                        img: ({ src, alt, ...props }) => {
+                            const resolvedSrc = `https://github.com/ayushmanchhabra/asyncawake.studio/blob/staging/img/${images[src as string] || src}`;
+                            return <img src={resolvedSrc} alt={alt} {...props}  style={{ width: '100%' }} />;
+                        },
+                    }}
+                >
+                    {post}
+                </Markdown>
             </div>
 
             <List>
